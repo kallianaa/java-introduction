91543927a1bed13245d188223768bfa9c4d6231f
kall!ana <kalliana.glaeser@gmail.com>
2026-07-18 19:59:52 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,68 @@
+import java.util.ArrayList;
 
 public class Main {
+
+    String nome;
+
    
-public static void main() {
-   int idade = 30;
-   int salario
-}
+    public static void main(String[] args) {
+    }
+        void declaraNome(){
+            nome = "Kalliana";
+        }
+
+        String getNome() {
+            return nome;
+        }
+
+
+        byte b = 100;
+        short s = 10000;
+        int j = 100000;
+        long l = 10000000000L;
+        float f = 10.5f;
+        double d = 20.99;
+        char c = 'A';
+        String str = "Hello, Java!";
+        boolean bool = true;
+
+
+        // int [] idade = new int[10];
+        // String [] nomes = new String[10];
+        // boolean [] saoBrasileiros = new boolean[10];
+
+        // ArrayList<Integer> idades = new ArrayList<Integer>();
+        // int[] idades1 = new int[10];
+        // idades1.length
+        // idades.add(22);
+        // idades.add(30);
+        // idades.remove(0);
+        // idades.get(1);
+        // idades.size();
+
+
+        // int idade = 10;
+        // for (var i = 0; i < idade; i++) {
+        // System.out.println("oi");
+        // }
+
+        // int i = 0;
+        // while (i < idade) {
+        //     i++;
+        //     System.out.println("oi");
+        // }
+
+
+        // int idade1 = 30;
+        // double idade2 = idade1;
+
+        // idade1 = (int) idade2;
+
+        // char letra = 'A';
+        // String nome = String.valueOf(letra);
+        // letra = nome.charAt(0);
+        // String nome2 = String.valueOf(idade1);
+        // idade1 = Integer.parseInt(nome2);
+
+
 }
